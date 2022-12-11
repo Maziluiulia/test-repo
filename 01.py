4da003cd21cc0ad02987821355aa20bed0c54a94
Maziluiulia <iulika.a4@gmail.com>
2022-12-11 10:54:01 +0200

    homework01

---
+++ 01.py
@@ -0,0 +1,31 @@
+# 1 item
+a = "I love Python"
+a *= 42
+print(a)
+
+# 2 item
+age_in_month = 361
+print(age_in_month)
+
+# 3 item
+age_in_years = age_in_month//12
+print(age_in_years)
+
+# 4 item
+my_age = f"My name is Iulia. I'm {age_in_years} years old"
+print(my_age)
+
+# 5 item
+i = 1
+print(i == 2)
+print(i <= 34)
+print(i > 12)
+print(i != 1)
+print(i >= 0)
+
+# 6 item
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+print(d)
