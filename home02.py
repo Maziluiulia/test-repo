51a26753f72476a2f460770ba63c1482177f669e
Maziluiulia <iulika.a4@gmail.com>
2022-12-16 20:55:00 +0200

    home02 added

---
+++ home02.py
@@ -0,0 +1,9 @@
+a = input("Write something: ")
+if a.isalpha():
+    print("It is text ")
+    print("Its length is")
+    print(len(a))
+elif (a.isnumeric()) and (int(a) % 2 == 0):
+     print("It is number and it is even")
+else:
+    print("It is number and it is odd")
