cf1e653206c57248d714f6e736ece45aead4a139
Maziluiulia <iulika.a4@gmail.com>
2022-12-17 19:46:25 +0200

    homework 02 update

--- home02.py
+++ home02.py
@@ -1,9 +1,9 @@
 a = input("Write something: ")
-if a.isalpha():
+if (a.isdigit()) and (int(a) % 2 == 0):
+    print("It is number and it is even")
+elif (a.isdigit()) and (int(a) % 2 != 0):
+    print("It is number and it is odd")
+else:
     print("It is text ")
     print("Its length is")
     print(len(a))
-elif (a.isdigit()) and (int(a) % 2 == 0):
-     print("It is number and it is even")
-else:
-    print("It is number and it is odd")
