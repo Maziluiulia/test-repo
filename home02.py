e5ef29806e38ee189d784f7900cd3a66a907b2e9
Maziluiulia <119345466+Maziluiulia@users.noreply.github.com>
2022-12-28 22:11:33 +0200

    Add files via upload

--- home02.py
+++ home02.py
@@ -1,9 +1,7 @@
 a = input("Write something: ")
-if (a.isdigit()) and (int(a) % 2 == 0):
+if a.isdigit() and int(a) % 2 == 0:
     print("It is number and it is even")
 elif (a.isdigit()) and (int(a) % 2 != 0):
     print("It is number and it is odd")
 else:
-    print("It is text ")
-    print("Its length is")
-    print(len(a))
+    print(f'It is a text. Its length is {len(a)}')
